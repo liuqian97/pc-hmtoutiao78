251413ab2cd05d3390f33b5f0c3c869c58c01602
liuqian <735759458@qq.comssh-keygen -t rsa -C 735759458@qq.com>
2019-08-01 22:15:48 +0800

    login页

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import router from '@/router'
+import '@/style/index.less'
 
+Vue.use(ElementUI)
+
+// 产品级别提示,选择不开始. 现在是开发级别提示,提示越详细越好
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
---
+++ src/views/login/index.vue
@@ -0,0 +1,29 @@
+<template>
+   <div class="container">
+       <el-card class="my-card">
+       </el-card>
+   </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang='less'>
+  .container {
+      width: 100%;
+      height: 100%;
+      position:absolute;
+      background:url(../../assets/images/login_bg.jpg) no-repeat center / cover;
+  }
+  .my-card {
+      width: 400px;
+      height: 350px;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%,-60%);
+  }
+</style>
