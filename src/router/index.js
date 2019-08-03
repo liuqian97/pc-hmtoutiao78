ed30d6f4a460a605bfb3925bee5580cb1b62a917
liuqian <735759458@qq.comssh-keygen -t rsa -C 735759458@qq.com>
2019-08-03 16:10:56 +0800

    首页欢迎页面完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,25 @@
 import VueRouter from 'vue-router'
 import Vue from 'vue'
 import Login from '@/views/login/index.vue'
+import Home from '@/views/home/index.vue'
+import Welcome from '@/views/welcome/index.vue'
 
 Vue.use(VueRouter)
 const router = new VueRouter({
   routes: [
     {
       path: '/login', name: 'login', component: Login
+    },
+    {
+      path: '/',
+      component: Home,
+      children: [
+        {
+          path: '/',
+          name: 'welcome',
+          component: Welcome
+        }
+      ]
     }
   ]
 })
