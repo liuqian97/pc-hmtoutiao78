86d9bff6f0ed62d18ca4211153c7a3d0bd2b22a2
liuqian <735759458@qq.comssh-keygen -t rsa -C 735759458@qq.com>
2019-08-03 13:51:05 +0800

    登录模块完成

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import router from '@/router'
 import '@/style/index.less'
-
+import axios from 'axios'
+Vue.prototype.$http = axios
 Vue.use(ElementUI)
 
 // 产品级别提示,选择不开始. 现在是开发级别提示,提示越详细越好
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,29 +1,104 @@
 <template>
-   <div class="container">
-       <el-card class="my-card">
-       </el-card>
-   </div>
+  <div class="container">
+    <el-card class="my-card">
+      <img src="../../assets/images/logo_index.png" alt />
+      <el-form ref="loginForm" status-icon :model="loginForm" :rules="loginRules">
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入您的手机号"></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-input
+            v-model="loginForm.code"
+            placeholder="请输入验证码"
+            style="width:237px;margin-right:10px"
+          ></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width:100%" @click="login()">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
+  data () {
+    var checkMobile = (rule, value, callback) => {
+      // value表示字段的值,callback回调函数,如果验证失败callback(new Error(错误信息)),如果验证正确,callback()
+      // 手机号格式 1开头 第二位3-9 最后为9位数字
+      if (!/^1[3-9]\d{9}$/.test(value)) {
+        return callback(new Error('手机格式不对'))
+      }
+      callback()
+    }
+    // 自定义验证码校验规则
+    var checkCode = (rule, value, callback) => {
+      if (!/^\d{6}$/.test(value)) {
+        return callback(new Error('请输入6位数字'))
+      }
+      callback()
+    }
+    return {
+      loginForm: {
+        mobile: '13662019984',
+        code: '246810'
+      },
+      loginRules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'blur' } // 自定义手机号校验规则
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          //   { len: 6, message: '请输入6位数字', trigger: 'blur' }
+          { validator: checkCode, trigger: 'blur' }
 
+        ]
+      }
+    }
+  },
+  methods: {
+    login () {
+      // validate是对象整个表单进行验证
+      // 获取其他组件里的函数,要通过dom对象调用
+      // this.$message.error是element-ui提供的方法,可以button传入不同type值时,调用
+      this.$refs.loginForm.validate(valid => {
+        if (valid) {
+          this.$http.post('http://ttapi.research.itcast.cn/mp/v1_0/authorizations', this.loginForm).then(result => {
+            this.$router.push('/')
+          }).catch(() => {
+            this.$message.error('手机号或者验证码错误')
+          })
+        }
+      })
+    }
+  }
 }
 </script>
 
 <style scoped lang='less'>
-  .container {
-      width: 100%;
-      height: 100%;
-      position:absolute;
-      background:url(../../assets/images/login_bg.jpg) no-repeat center / cover;
+.container {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background: url(../../assets/images/login_bg.jpg) no-repeat center / cover;
+}
+.my-card {
+  width: 400px;
+  height: 350px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -60%);
+  img {
+    display: block;
+    width: 200px;
+    margin: 0 auto 20px;
   }
-  .my-card {
-      width: 400px;
-      height: 350px;
-      position: absolute;
-      left: 50%;
-      top: 50%;
-      transform: translate(-50%,-60%);
-  }
+}
 </style>
