17a32db3450d52a9396d709f76733aa42f605ef4
liuqian <735759458@qq.comssh-keygen -t rsa -C 735759458@qq.com>
2019-08-04 21:26:25 +0800

    导航菜单路由and404页面

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,9 @@ import Vue from 'vue'
 import Login from '@/views/login/index.vue'
 import Home from '@/views/home/index.vue'
 import Welcome from '@/views/welcome/index.vue'
+import Article from '@/views/article/index.vue'
+import Image from '@/views/images/index.vue'
+import notFound from '@/views/404/index.vue'
 
 Vue.use(VueRouter)
 const router = new VueRouter({
@@ -18,8 +21,23 @@ const router = new VueRouter({
           path: '/',
           name: 'welcome',
           component: Welcome
+        },
+        {
+          path: '/article',
+          name: 'article',
+          component: Article
+        },
+        {
+          path: '/image',
+          name: 'image',
+          component: Image
         }
       ]
+    },
+    {
+      path: '*',
+      name: '404',
+      component: notFound
     }
   ]
 })
---
+++ src/views/images/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>images</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
